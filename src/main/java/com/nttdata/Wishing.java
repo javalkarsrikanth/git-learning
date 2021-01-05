d4cee208f7006da381651fdcc5c25035a33e8b54
Srikanth <srikanthjavalkar@gmail.com>
2021-01-05 18:01:50 +0530

    Goodmorning branch

--- src/main/java/com/nttdata/Wishing.java
+++ src/main/java/com/nttdata/Wishing.java
@@ -4,6 +4,5 @@ public class Wishing {
     public static void main(String[] args) {
 
         System.out.println("Hi this is srikanth. Good Morning");
-        System.out.println("Hi this is srikanth. Good evening");
     }
 }
